15f8a18572989361606a4248b0025c9adc1832a0
Allen Wu <Allen.Wu@originate.com>
2013-06-21 17:43:33 -0700

    Added unit tests

---
+++ VoiceRecordTests/VoiceRecordTests.h
@@ -0,0 +1,13 @@
+//
+//  VoiceRecordTests.h
+//  VoiceRecordTests
+//
+//  Created by Allen Wu on 6/21/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface VoiceRecordTests : SenTestCase
+
+@end
---
+++ VoiceRecordTests/VoiceRecordTests.m
@@ -0,0 +1,34 @@
+//
+//  VoiceRecordTests.m
+//  VoiceRecordTests
+//
+//  Created by Allen Wu on 6/21/13.
+//
+//
+
+#import "VoiceRecordTests.h"
+
+@implementation VoiceRecordTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+  int two = 2;
+  int two2 = 2;
+  STAssertEquals(two + two2, 4, @"two plus two equals four");
+}
+
+@end
